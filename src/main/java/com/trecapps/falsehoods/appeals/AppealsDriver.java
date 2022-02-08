53b2217eaee100409789b344fc82d3999b68609a
John Jacko <jljacko@outlook.com>
2022-02-07 19:59:01 -0600

    Started Appeals Repository

---
+++ src/main/java/com/trecapps/falsehoods/appeals/AppealsDriver.java
@@ -0,0 +1,11 @@
+package com.trecapps.falsehoods.appeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppealsDriver {
+    public static void main(String[] args) {
+        SpringApplication.run(AppealsDriver.class, args);
+    }
+}
